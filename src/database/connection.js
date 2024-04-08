fb001836869d905235f98ccfd67ff2377322ba09
Francisco Zuidwijk <electrocisko@gmail.com>
2024-04-08 14:58:32 -0300

    actualizado imagenes

--- src/database/connection.js
+++ src/database/connection.js
@@ -4,7 +4,7 @@ import config from "../config/dotenvConfig.js";
 const MONGO_USER = config.database.MONGO_USER;
 const MONGO_PASSWORD = config.database.MONGO_PASSWORD;
 
-const MONGO_URI = `mongodb+srv://${MONGO_USER}:${MONGO_PASSWORD}@cluster0.rvl2uyz.mongodb.net/app-bordados-dev?retryWrites=true&w=majority`;
+const MONGO_URI = `mongodb+srv://${MONGO_USER}:${MONGO_PASSWORD}@cluster0.rvl2uyz.mongodb.net/app-bordados?retryWrites=true&w=majority`;
 
 const connection = async () => {
     try {
